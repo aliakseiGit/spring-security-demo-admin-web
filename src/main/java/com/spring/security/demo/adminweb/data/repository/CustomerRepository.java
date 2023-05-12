361919119b293280e0006464049efbbf16f71f19
EPAM\Aliaksei_Astapenka1 <aliaksei_astapenka1@epam.com>
2023-05-12 10:29:38 +0200

    add authorization

--- src/main/java/com/spring/security/demo/adminweb/data/repository/CustomerRepository.java
+++ src/main/java/com/spring/security/demo/adminweb/data/repository/CustomerRepository.java
@@ -1,5 +1,6 @@
 package com.spring.security.demo.adminweb.data.repository;
 
+
 import com.spring.security.demo.adminweb.data.model.Customer;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
